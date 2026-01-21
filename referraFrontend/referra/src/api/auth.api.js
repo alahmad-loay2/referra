0e50506206190da77f5171d3a4b83fdd1022f29a
alahmad-loay2 <loay.alahmad3@gmail.com>
2026-01-21 22:48:39 +0200

    deployment fix 3

--- referraFrontend/referra/src/api/auth.api.js
+++ referraFrontend/referra/src/api/auth.api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5500/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5500/api'
 
 export const signup = async (payload) => {
   const res = await fetch(`${API_BASE_URL}/auth/signup`, {
