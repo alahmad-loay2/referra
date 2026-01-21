da9bc239e3118a0f6845aeec2ebc29a20bd79002
alahmad-loay2 <loay.alahmad3@gmail.com>
2026-01-21 23:01:48 +0200

    fix 5 deployment

--- referraBackend/app.js
+++ referraBackend/app.js
@@ -2,21 +2,21 @@ import express from 'express'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { FRONTEND_URL } from './config/env.js'
 import authRoutes from './routes/auth.routes.js'
 import errorMiddleware from './middleware/error.middleware.js'
+import { FRONTEND_URL } from './config/env.js'
 
 const app = express()
 
-
-app.use(helmet())
+app.set("trust proxy", 1);
 app.use(cookieParser())
-app.use(express.json())
+app.use(helmet())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: FRONTEND_URL,
     credentials: true
 }))
+app.use(express.json())
 
 app.use("/api/health" , (req, res) => {
     res.status(200).send("OK")
