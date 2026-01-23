9c13f376eeeed2683543cfe0fb65c31e4bc658af
alahmad-loay2 <loay.alahmad3@gmail.com>
2026-01-23 12:56:01 +0200

    adding positions frontend

--- referraFrontend/referra/src/pages/employeeDashboard/EmployeePositions/EmployeePositions.jsx
+++ referraFrontend/referra/src/pages/employeeDashboard/EmployeePositions/EmployeePositions.jsx
@@ -5,5 +5,4 @@ const EmployeePositions = () => {
     <div>EmployeePositions</div>
   )
 }
-
 export default EmployeePositions
--- referraFrontend/referra/src/pages/hrDashboard/HrPositions/HrPositions.jsx
+++ referraFrontend/referra/src/pages/hrDashboard/HrPositions/HrPositions.jsx
@@ -1,10 +1,63 @@
 import React from "react";
-
+import "./HrPositions.css";
+import { Briefcase, Users, Layers } from "lucide-react"
 const HrPositions = () => {
   return (
-    <div>
-      <h1>Positions</h1>
-      <p>View and manage open and filled positions here.</p>
+    <div className="positionsHR">
+
+      <div className="positionCardsContainer">
+        <div className="positionCards">
+          <div className="positionsIcon">
+          <Briefcase size={20} color="white"/>
+          </div>
+          <div className="positionCardsText">
+          <p>Total Positions</p>
+          <span>2</span>
+          </div>
+        </div>
+        <div className="positionCards">
+          <div className="positionsIcon">
+          <Users  size={20} color="white"/>
+          </div>
+          <div className="positionCardsText">
+          <p>Total Positions</p>
+          <span>2</span>
+          </div>
+        </div>
+        <div className="positionCards">
+          <div className="positionsIcon">
+          <Layers size={20} color="white" />
+          </div>
+          <div className="positionCardsText">
+          <p>Total Positions</p>
+          <span>2</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="positionsSearchContainer">
+        <input
+          type="text"
+          className="searchPositionInput"
+          placeholder="Search for positions"
+        />
+        <select name="departments">
+          <option value="all">All Departments</option>
+          <option value="hr">HR</option>
+          <option value="it">IT</option>
+          <option value="sales">Sales</option>
+        </select>
+        <select name="status">
+          <option value="all">All Status</option>
+          <option value="open">Open</option>
+          <option value="closed">Closed</option>
+        </select>
+      </div>
+
+      <div className="positionsTableContainer">
+        
+      </div>
+
     </div>
   );
 };
